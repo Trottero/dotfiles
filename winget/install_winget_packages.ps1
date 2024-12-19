dd40fd1f455d0964b6f396bdcb203b9e8c07acc0
Trottero <wrlxs@hotmail.com>
2024-12-19 15:47:10 +0100

    Add winget packages

---
+++ winget/install_winget_packages.ps1
@@ -0,0 +1,11 @@
+winget install JanDeDobbeleer.OhMyPosh -s winget
+winget install -e --id Microsoft.VisualStudioCode
+winget install --id Git.Git -e -s winget
+winget install --id=Microsoft.DotNet.HostingBundle.6 -e
+winget install --id=Microsoft.DotNet.HostingBundle.8 -e
+winget install --id=Microsoft.DotNet.HostingBundle.9 -e
+winget install Microsoft.DotNet.SDK.6
+winget install Microsoft.DotNet.SDK.8
+winget install Microsoft.DotNet.SDK.9
+
+winget install -e --id Microsoft.Azure.FunctionsCoreTools
